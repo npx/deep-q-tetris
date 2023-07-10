fec75bfac433ace43fdcc120064c99334497217c
npx <>
2023-07-10 16:13:02 +0200

    feat(pieces): add offset information to pieces

--- pieces.py
+++ pieces.py
@@ -1,10 +1,10 @@
-from i_piece import i_piece
-from j_piece import j_piece
-from l_piece import l_piece
-from o_piece import o_piece
-from s_piece import s_piece
-from t_piece import t_piece
-from z_piece import z_piece
+from i_piece import i_piece, i_piece_offset_width, i_piece_offset_height
+from j_piece import j_piece, j_piece_offset_width, j_piece_offset_height
+from l_piece import l_piece, l_piece_offset_width, l_piece_offset_height
+from o_piece import o_piece, o_piece_offset_width, o_piece_offset_height
+from s_piece import s_piece, s_piece_offset_width, s_piece_offset_height
+from t_piece import t_piece, t_piece_offset_width, t_piece_offset_height
+from z_piece import z_piece, z_piece_offset_width, z_piece_offset_height
 
 PIECES = {
     "i": i_piece,
@@ -14,6 +14,26 @@ PIECES = {
     "s": s_piece,
     "t": t_piece,
     "z": z_piece,
+}
+
+OFFSET_WIDTH = {
+    "i": i_piece_offset_width,
+    "j": j_piece_offset_width,
+    "l": l_piece_offset_width,
+    "o": o_piece_offset_width,
+    "s": s_piece_offset_width,
+    "t": t_piece_offset_width,
+    "z": z_piece_offset_width,
+}
+
+OFFSET_HEIGHT = {
+    "i": i_piece_offset_height,
+    "j": j_piece_offset_height,
+    "l": l_piece_offset_height,
+    "o": o_piece_offset_height,
+    "s": s_piece_offset_height,
+    "t": t_piece_offset_height,
+    "z": z_piece_offset_height,
 }
 
 
@@ -35,3 +55,13 @@ def get_dim(piece):
 def get_blocks(piece):
     name, rotation = piece
     return PIECES[name][rotation]
+
+
+def offset_width(piece):
+    name, rotation = piece
+    return OFFSET_WIDTH[name][rotation]
+
+
+def offset_height(piece):
+    name, rotation = piece
+    return OFFSET_HEIGHT[name][rotation]
