461c13ac8ab7c89d14386a7b41fa408ef57cbd5d
npx <>
2023-07-25 09:42:34 +0200

    feat(network): paper arch

--- deep_q_network.py
+++ deep_q_network.py
@@ -1,4 +1,5 @@
 # mypy: ignore-errors
+import torch
 import torch.nn as nn
 
 
@@ -6,37 +7,62 @@ class DeepQNetwork(nn.Module):
     def __init__(self):
         super(DeepQNetwork, self).__init__()
 
-        # 2d conv network
-        # kernel for convolution chosen with padding to keep original board size
-        # channels is 1 because we dont have an RGB 3 channel image
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 128, (7, 5), padding=(3, 2)), nn.ReLU(inplace=True))
-        # -> 21x10x64
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(128, 128, (3, 3), padding=(1, 1)), nn.ReLU(inplace=True))
-        # -> 18x7x1
-        # -> 21x10x1
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(128, 1, (3, 3), padding=(1, 1)), nn.ReLU(inplace=True))
-        self.conv4 = nn.Sequential(nn.Linear(21*10, 1))
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(
+            in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(
+            in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(
+            in_channels=32, out_channels=64, kernel_size=3, padding=1)
 
-        self._create_weights()
+        # Layer to collapse each column into a single pixel with 64 feature channels
+        self.collapse = nn.Conv2d(
+            in_channels=64, out_channels=64, kernel_size=(1, 10))
 
-    def _create_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_uniform_(m.weight)
-                nn.init.constant_(m.bias, 0)
+        # More Convolutional layers
+        self.conv4 = nn.Conv2d(
+            in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(
+            in_channels=128, out_channels=128, kernel_size=1)
+        self.conv6 = nn.Conv2d(
+            in_channels=128, out_channels=128, kernel_size=3, padding=1)
+
+        # Fully connected layers
+        self.fc1 = nn.Linear(128, 128)
+        self.fc2 = nn.Linear(128, 512)
+        self.fc3 = nn.Linear(512, 1)
+
+        # ReLU non-linearity
+        self.relu = nn.ReLU()
+
+        self.dropout = nn.Dropout(0.25)
 
     def forward(self, x):
-        # add one dimension / pseudo channel
         x = x.unsqueeze(1)
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        # flatten 2d vector for linear layer
-        # -1: determine batch size automatically
-        x = x.view(-1, 21*10)
-        x = self.conv4(x)
+
+        # Convolutional layers with ReLU and BatchNorm
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+
+        # Collapse columns into a single pixel
+        x = self.collapse(x)
+
+        # More Convolutional layers with ReLU and BatchNorm
+        x = self.relu(self.conv4(x))
+        x = self.relu(self.conv5(x))
+        x = self.relu(self.conv6(x))
+
+        # Global average pooling across spatial dimensions
+        x = torch.mean(x, dim=(2, 3))
+
+        # Fully connected layers with ReLU and Dropout
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc2(x))
+        x = self.dropout(x)
+
+        # Final fully connected layer
+        x = self.fc3(x)
 
         return x
